282a7182e719d2177219a80ac844823716a385ee
Fredrik <fredrik.persson@yh.nackademin.se>
2020-04-21 10:50:37 +0200

    Add postprocessing
    Adding postprocessing effects and more folder like prefabs and postproessing. Setting up a simple post processing example in the test scene

---
+++ Assets/Scripts/Interfaces/block_z.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Interface for prefabs handle with block
+// z stands for zaid periodic element ;-)
+[System.Serializable]
+public class block_z : MonoBehaviour
+{
+    public string Name;
+    public GameObject Element;
+}
