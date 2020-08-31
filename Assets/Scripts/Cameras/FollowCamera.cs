d3b153a41c7c41c1c0e05f0b7dfef49fef8df5ef
Fredrik <fredrik.persson@yh.nackademin.se>
2020-08-31 14:13:40 +0200

    change

---
+++ Assets/Scripts/Cameras/FollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Follow camera is a class that handle object to follow the camera basically.
+// And it use physics to calculate a smooth hinge/join between tho.
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        // only using these 3 line, is useful for side platform game cameras. 
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
